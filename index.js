cb35ef6479d4196f64ae927b5ccc01c6ab9bc3db
atheninha2 <ungerfigueiredo@gmail.com>
2024-05-01 22:39:55 -0300

    remove nodeModules

--- index.js
+++ index.js
@@ -21,14 +21,10 @@ app.listen(PORT, () => {
 });
 // Obtendo os parametros passados pela linha de comando
 var userArgs = process.argv.slice(2);
-var mongoURL =
-  "mongodb+srv://joao226793:pepe190105@cluster0.vqdns90.mongodb.net/";
+var mongoURL = userArgs[0];
 //Configurando a conexao com o Banco de Dados
 var mongoose = require("mongoose");
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(mongoURL);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on("error", (error) => {
